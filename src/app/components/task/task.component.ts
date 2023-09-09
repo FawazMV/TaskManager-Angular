14c42827f8deeae921f32631026a7e45af6e4a2b
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-09-09 15:55:18 +0530

    to-to app ready to build

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { TaskService } from 'src/app/services/task.service';
-import { Task } from 'src/Model/Task';
+import { Component, OnInit } from '@angular/core'
+import { TaskService } from 'src/app/services/task.service'
+import { Task } from 'src/Model/Task'
 
 @Component({
   selector: 'app-task',
@@ -10,36 +10,26 @@ import { Task } from 'src/Model/Task';
 export class TaskComponent implements OnInit {
   tasks: Task[] = []
 
-  constructor(private service: TaskService) { }
+  constructor (private service: TaskService) {}
 
-  ngOnInit() {
+  ngOnInit () {
     this.tasks = this.service.getTask()
   }
 
-  deleteTask(task: Task) {
+  deleteTask (task: Task) {
     let res = confirm('Are you sure you want to delete?')
     if (!res) return
     if (this.service.deleteTask(task))
       this.tasks = this.tasks.filter(t => t.id !== task.id)
-
   }
 
-  clearAllTask() {
-    this.service.clearTask()
-    this.tasks = []
-  }
-
-  onAddNewTask(newTask: Task) {
+  onAddNewTask (newTask: Task) {
     this.tasks.unshift(this.service.addItem(newTask))
   }
 
-  toggleReminder(task: Task) {
-    if (this.service.updateTask(task))
-      task.isCompleted = !task.isCompleted;
-    if (task.isCompleted)
-      alert(task.text + ' is registered as completed');
-    else
-      alert(task.text + ' is registered as incompleted');
+  toggleReminder (task: Task) {
+    if (this.service.updateTask(task)) task.isCompleted = !task.isCompleted
+    if (task.isCompleted) alert(task.text + ' is registered as completed')
+    else alert(task.text + ' is registered as incompleted')
   }
-
 }
